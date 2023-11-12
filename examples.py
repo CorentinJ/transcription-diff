5d87906b05f6fd9c3bb360a6710cbf4119f25240
CorentinJ <corentin.jemine@gmail.com>
2023-11-12 12:21:19 +0100

    Ensuring we span the whole unnormalized text

--- examples.py
+++ examples.py
@@ -14,7 +14,7 @@ wav, sr = librosa.core.load(audio_fpath)
 # We'll keep only a short audio to keep the demo concise
 cut_range = 2.5, 5.25
 wav = wav[int(sr * cut_range[0]):int(sr * cut_range[1])]
-correct_transcription = "It befell in the month of May, Queen Guenever called to her knights of the Table Round"
+correct_transcription = "It befell in the month of May, Queen Guenever called to her knights of the Table Round."
 
 # # You can listen to the audio using this package, or by playing the file at <audio_fpath>
 # import sounddevice as sd
